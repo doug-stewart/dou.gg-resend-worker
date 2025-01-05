6136a838d5fbd8da4ee087e946727ef157fbd3cf
Doug Stewart <hi@dou.gg>
2025-01-04 21:42:32 -0800

    Use secret Resend API Key

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ import { Resend } from 'resend';
 
 export default {
 	async fetch(request, env, ctx) {
-		const resend = new Resend('your_resend_api_key');
+		const resend = new Resend(env.RESEND_API_KEY);
 
 		const { data, error } = await resend.emails.send({
 			from: 'contact-form@dou.gg',
